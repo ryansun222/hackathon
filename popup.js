65d4e55be48c24ce93737d889c8901d95a84c3f3
Ryan Sun <ryansun222@gmail.com>
2023-03-30 11:07:09 -0700

    popup.js updated

--- popup.js
+++ popup.js
@@ -1,12 +1,25 @@
+// add slider to popup.html
+const div = document.createElement('div');
+div.classList.add('slideContainer');
+const input = document.createElement('input');
+input.setAttribute('type', 'range');
+input.setAttribute('min', '0');
+input.setAttribute('max', '100');
+input.setAttribute('value', '50');
+input.setAttribute('id', 'myRange')
+document.querySelector('.controls').appendChild(div);
+div.appendChild(input);
+console.log('hello');
 
-// add slider to control hslColor
 
+// add slider functionality to chrome tabs
 
-const slider = document.getElementById("brightness-slider");
+const slider = document.getElementById("myRange");
 slider.addEventListener("input", () => {
-  const brightness = slider.value / 100; 
-  const hslColor = tinycolor(color).toHsl();
-  hslColor.l = brightness;
-  const newColor = tinycolor(hslColor).toHexString();
-  el.style.color = newColor;
+  const brightness = slider.value / 100;
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { brightness }, function (response) {
+      console.log(response);
+    });
+  });
 });
